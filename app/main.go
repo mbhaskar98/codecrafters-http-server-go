0285220975c94ed592cf6f45f301f651ebdbc9f2
Bhaskar Mahajan <bhaskarmahajan98@gmail.com>
2025-04-17 05:03:22 +0530

    [any message]

--- app/main.go
+++ app/main.go
@@ -15,16 +15,28 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	
+
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	//bytes, err := conn.Write([]byte("HTTP/1.1 200 OK\\r\\n\\r\\n"))
+	bytes, err := conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Bytes sent:", bytes)
+	err = conn.Close()
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
 		os.Exit(1)
 	}
 }
