6b57c03f3b95f78f6ecb75e7c18f77d3e93b0415
Bhaskar Mahajan <bhaskarmahajan98@gmail.com>
2025-04-25 04:52:21 +0530

    basic request response

--- app/httpServer/requestHandler.go
+++ app/httpServer/requestHandler.go
@@ -3,6 +3,7 @@ package httpServer
 import (
 	"fmt"
 	"github.com/codecrafters-io/http-server-starter-go/app/httpServer/constants"
+	"strings"
 )
 
 type RequestHandler struct{}
@@ -11,19 +12,22 @@ func (r *RequestHandler) HandleRequest(request HttpRequest) (*HttpResponse, erro
 	status := constants.ERROR_CODE_OK
 	message := constants.STATUS_OK
 
-	if request.Path != nil && *request.Path != "/" {
+	var body = new(string)
+	if request.Path == nil {
+		status = constants.ERROR_CODE_NOT_FOUND
+		message = constants.STATUS_NOT_FOUND
+	} else if strings.HasPrefix(*request.Path, "/echo") {
+		*body = strings.TrimPrefix(*request.Path, "/echo")
+	} else if *request.Path != "/" {
 		status = constants.ERROR_CODE_NOT_FOUND
 		message = constants.STATUS_NOT_FOUND
 	}
-	var body *string = nil
 	response := &HttpResponse{
 		Status: status,
-		//Body:   []byte("Hello World"),
-		//Headers: Header{
-		//	"Content-Type": []string{"text/plain"},
-		//  "Connection": []string{"close"},
-		//  "Connection": []string{"close"}
-		//},
+		Headers: Header{
+			"Content-Type": []string{"text/plain"},
+			"Connection":   []string{"close"},
+		},
 		Version: constants.HTTP_VERSION_1_1,
 		Message: message,
 		Reason:  message,
@@ -31,6 +35,7 @@ func (r *RequestHandler) HandleRequest(request HttpRequest) (*HttpResponse, erro
 	}
 
 	if body != nil && len(*body) > 0 {
+		response.Body = []byte(*body)
 		response.Headers["Content-Length"] = []string{fmt.Sprintf("%d", len(*body))}
 	}
 
